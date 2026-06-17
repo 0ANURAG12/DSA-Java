578ad8f1207d830bdb0b82fbc30434d7a7da6304
Anurag <aanuragyadav242@gmail.com>
2026-06-17 21:48:18 +0530

    Add solution for Invert Binary Tree problem
    Implemented three approaches to invert a binary tree: recursive DFS, iterative DFS using a stack, and iterative BFS using a queue.

---
+++ Trees/226. Invert Binary Tree.java
@@ -0,0 +1,143 @@
+/**
+ * LeetCode 226. Invert Binary Tree
+ * https://leetcode.com/problems/invert-binary-tree/
+ *
+ * Tags:
+ * Binary Tree
+ * DFS
+ * Recursion
+ *
+ * Time Complexity: O(N)
+ * N = Number of nodes in the tree.
+ *
+ * Space Complexity: O(H)
+ * H = Height of the tree (recursive call stack).
+
+# LeetCode 226. Invert Binary Tree
+
+**Problem Link:** https://leetcode.com/problems/invert-binary-tree/
+
+## Approach 1: Recursive DFS
+
+**Time Complexity:** O(N)
+
+**Space Complexity:** O(H)
+
+where:
+
+* N = Number of nodes in the tree
+* H = Height of the tree
+
+
+*/
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+/*
+
+---
+
+## Approach 2: Iterative DFS (Stack)
+
+**Time Complexity:** O(N)
+
+**Space Complexity:** O(H)
+
+where:
+
+* N = Number of nodes in the tree
+* H = Height of the tree
+
+*/
+import java.util.Stack;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return root;
+    }
+}
+/*
+
+
+---
+
+## Approach 3: Iterative BFS (Queue)
+
+**Time Complexity:** O(N)
+
+**Space Complexity:** O(W)
+
+where:
+
+* N = Number of nodes in the tree
+* W = Maximum width of the tree
+
+*/
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return root;
+    }
+}
+
